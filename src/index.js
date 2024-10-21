34cb39473c40b132317e14445a4e99c35321cf1c
jonathan <jonathankanaiya@gmail.com>
2024-10-21 14:13:46 +0300

    basic skeleton fetch again

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+document.querySelector("button").addEventListener("click", () => getWeather());
+
+async function getWeather() {
+  const w = document.querySelector("input").value;
+
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${w}?key=XZKP4PWEN6XB5C6TE8QLW2Z3V`,
+    { mode: "cors" }
+  )
+    .then((response) => {
+      if (response.ok) {
+        console.log(response.json());
+      }
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(() => {
+      console.log("error");
+    });
+}
